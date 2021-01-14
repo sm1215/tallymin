887eb897ed724136d867e49f452b040429c3763c
Sam Miller <sm1215@gmail.com>
2021-01-14 18:32:30 -0500

    outline some basic roulette plans

---
+++ js/roulette.js
@@ -0,0 +1,17 @@
+const roulette = {
+  highlightClass: '.tabulator-highlight',
+  winnerClass: 'tabulator-winner',
+  spinLengthMin: 2000,
+  spinLengthMax: 4000,
+  getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+  },
+  run: function(options) {
+    const spinLength = this.getRandom(this.spinLengthMin, this.spinLengthMax);
+    // TODO: randomly choose a winner from the options provided
+    // need to figure out what structure we can pull in from the rows selected by tabulator.js
+
+    // cycle through rows adding and removing the highlightClass as needed
+    // cycle multiple times, decreasing the spinLength each time
+  }
+}
