b1d4d3b105a63b1f15fe5dffa4a185c6c57a40bd
Sam Miller <sm1215@gmail.com>
2021-01-16 18:41:13 -0500

    work out the roulette rotation

--- js/roulette.js
+++ js/roulette.js
@@ -10,16 +10,18 @@ const roulette = {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
   },
-  run: async function(rowElements) {
-    // this.winner = this.getRandomIntInclusive(0, rowElements.length - 1);
-    this.winner = 1;
-    // TODO: need to adjust this so the loops end right next to the winning row
-    // probably mod by elements.length
-    const loops = this.getRandomIntInclusive(3, 9) * this.winner; 
+  run: async function(elements) {
+    this.winner = this.getRandomIntInclusive(0, elements.length - 1);
+    let highlights = this.getRandomIntInclusive(elements.length * 2, elements.length * 5);
+    let offset = elements.length + this.winner;
+    let remainder = highlights % offset;
+    let finalHighlights = highlights + remainder;
+
+    // reset the starting point
     this.elementIndex = 0;
 
-    await this.spin(loops, rowElements).then(async result => {
-      await this.spin(3, [rowElements[this.winner]], true);
+    await this.spin(finalHighlights, elements).then(async result => {
+      await this.spin(5, [elements[this.winner]], true);
     });
   },
   spin: async function(loops, elements, isWinner) {
@@ -40,7 +42,6 @@ const roulette = {
   },
   highlight: async function(element, isWinner) {
     const addClass = isWinner ? this.winnerClass : this.highlightClass;
-    console.log("addClass", addClass);
     return await new Promise(resolve => {
       setTimeout(() => {
         element.classList.add(addClass);
